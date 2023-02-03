8507e8f74de7cc9b6640385a9c083da7ab3ae06c
shangjie-li <1814481684@qq.com>
2023-02-03 10:38:20 +0800

    Add some comments

--- utils/loss_utils.py
+++ utils/loss_utils.py
@@ -2,20 +2,37 @@ import torch
 
 
 def focal_loss(inputs, targets, gamma=2.0):
-    pos_inds = targets.eq(1).float()
-    neg_inds = targets.lt(1).float()
+    """
+    For a ground truth heatmap, like
+        0.0 0.6 0.8 0.4 0.0
+        0.3 0.7 1.0 0.7 0.1
+        0.0 0.4 0.8 0.2 0.0
+    the positive and negative mask are
+        0.0 0.0 0.0 0.0 0.0    1.0 1.0 1.0 1.0 1.0
+        0.0 0.0 1.0 0.0 0.0    1.0 1.0 0.0 1.0 1.0
+        0.0 0.0 0.0 0.0 0.0    1.0 1.0 1.0 1.0 1.0
+    the focal loss makes the network learn to predict the keypoint.
 
-    neg_weights = torch.pow(1 - targets, 4)
-    pos_loss = torch.log(inputs) * torch.pow(1 - inputs, gamma) * pos_inds
-    neg_loss = torch.log(1 - inputs) * torch.pow(inputs, gamma) * neg_inds * neg_weights
+    Args:
+        inputs: tensor of float32, [B, C, H, W], predicted heatmap (from 0 to 1)
+        targets: tensor of float32, [B, C, H, W], ground truth heatmap (from 0 to 1)
+        gamma: float
 
-    pos_loss = pos_loss.sum()
-    neg_loss = neg_loss.sum()
+    Returns:
+        loss: tensor of float, []
 
-    num_pos = pos_inds.float().sum()
-    if num_pos == 0:
-        loss = -neg_loss
-    else:
-        loss = -(pos_loss + neg_loss) / num_pos
+    """
+    pos_mask = targets.eq(1).float()
+    neg_mask = targets.lt(1).float()
 
-    return loss.mean()
+    pos_loss = pos_mask * torch.log(inputs) * torch.pow(1 - inputs, gamma)
+    neg_loss = neg_mask * torch.log(1 - inputs) * torch.pow(inputs, gamma) * torch.pow(1 - targets, 4)
+
+    pos_loss = -pos_loss.sum()
+    neg_loss = -neg_loss.sum()
+
+    loss = pos_loss + neg_loss
+    num_pos = pos_mask.float().sum()
+    loss /= num_pos if num_pos >= 1.0 else 1.0
+
+    return loss
